6d6a6373fd19c09eff8518f0bd7ed197365c749e
EstebanHR <estebanhr1981@hotmail.com>
2021-02-16 03:06:26 +0000

    Ejercicio Card

--- src/main.js
+++ src/main.js
@@ -6,5 +6,23 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  console.log("Hello Rigo from the console!");
+  let numerosPosible = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+  ];
+
+  let palos = ["diamonds", "hearts", "clubs", "spades"] 
+  let numAleatorio = Math.floor((Math.random() *13));
+
 };
